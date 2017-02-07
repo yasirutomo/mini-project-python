d7ece183b8110531706071fbaf05e4934ba028fd
yasirutomo <yasirutomo@gmail.com>
2017-02-08 05:35:39 +0700

    permainan lempar dadu (dice rolling simulator)

---
+++ lempardadu.py
@@ -0,0 +1,18 @@
+import random
+
+def roll(sides=6):
+	lempardadu = random.randint(1,sides)
+	return lempardadu
+
+def main():
+	putar = True
+	while putar:
+		tanya = str(input('\nIngin melempar dadu? [y/n]'))
+		if tanya.lower() != 'n':
+			dadu = roll()
+			print('Dadu yang keluar: ', dadu)
+		else:
+			putar = False
+	print('\nTerimakasih Sudah Bermain')
+
+main()
